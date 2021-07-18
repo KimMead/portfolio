0bfd735c7b1aec58fcb3a7bf3fbb90c766bf06bd
KimMead <kimmeadows8@gmail.com>
2021-07-18 12:25:50 -0500

    Add Material icons module, add email and phone

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,23 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import { Person, Mail } from '@material-ui/icons';
 
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">kim meadows.</a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>654654</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>kimmeadows8@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right"></div>
+            </div>
         </div>
     )
 }
