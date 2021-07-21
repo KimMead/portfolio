b23c975677d784c73951306dca4983e67ae72b6b
KimMead <kimmeadows8@gmail.com>
2021-07-20 21:39:48 -0500

    Add portfolio examples

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App 1</li>
+                <li>Web App 2</li>
+                <li>Web App 3</li>
+                <li>Web App 4</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt=""/>
+                    <h3></h3>
+                </div>
+            </div>
         </div>
     )
 }
