51db87826a5d057eb2232d4266ac76f8acc4018a
KimMead <kimmeadows8@gmail.com>
2021-07-22 13:15:04 -0500

    add slider arrows to works section

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -18,9 +18,15 @@ export default function Works() {
                             <span>Projects</span>
                             </div>
                         </div>
-                    <div className="right"></div>
+                    <div className="right">
+                        <img
+                            src="" alt=""/>
+                    </div>
                 </div>
             </div>
         </div>
+        <img src="assets/arrow.png" className="arrow left" alt=""/>
+        <img src="assets/arrow.png" className="arrow right" alt=""/>
     </div>
+    
 }
